c4d42122dea3461ad0c086ff21e05137721a31a2
Indigo <indigoclay@gmail.com>
2019-09-20 13:15:26 +0200

    modifications

--- ttest_display.m
+++ ttest_display.m
@@ -1,23 +1,35 @@
-z = []
-samples = 3;
+tvals = []
+m = []
+samples = 5;
 rootsamples = sqrt(samples);
 
 for i = 1:10000
   xs = stdnormal_rnd(samples,1);
   s = std(xs);
   t = rootsamples*mean(xs)/s;
-  z(i) = t;
+  tvals(i) = t;
+  m(i) = mean(xs);
 endfor
-#z = randn(10000,1);
-hold on;
-[f,x] = hist(z,500);
 
-#q = hist(z,30, 1);
-#qs = sum(q);
+hold on;
+[f,x] = hist(tvals,500);
+
+[fm,xm] = hist(m,50);
+
+# ---- distribution of  the sample means
+
+dxm = diff(xm(1:2));
+AreaM = dxm * sum(fm)
+bar(xm,fm/AreaM)
+
+# ---- distribution of t values
+
 dx = diff(x(1:2));
 Area = dx * sum(f);
-NewArea = dx* sum(f/Area)
-bar(x,f/Area)
+bar(x,f/Area);
+
+# ----- t-distirubtion and normal distribution
+
 x=[-4:0.1:4];
 plot (x,normpdf(x,0,1));
 plot(x, tpdf(x,samples));
